64251847a6f5c2a77ef65f645c92c330bc0a8116
dynamitesushi <winson.hero@gmail.com>
2023-06-09 11:39:10 +0100

    first time splash screen now uses app uid

--- src/pages/Splash/index.tsx
+++ src/pages/Splash/index.tsx
@@ -1,22 +1,26 @@
 import * as React from 'react';
 import Lottie from 'lottie-react';
 import splashAnimation from '../../splashAnimation.json';
+import getAppUID from '../../utilities/getAppUID';
 
 const Splash = () => {
+  const localStorageItemName = '__block_splash';
   const [displaySplash, setDisplaySplash] = React.useState(false);
 
   React.useEffect(() => {
-    const splash = localStorage.getItem('__block_splash');
+    const appUID = getAppUID();
+    const splash = localStorage.getItem(localStorageItemName);
 
-    if (!splash) {
+    if (!splash || splash !== appUID) {
       setDisplaySplash(true);
     }
   }, []);
 
   React.useEffect(() => {
     const timeout = setTimeout(() => {
+      const appUID = getAppUID();
       setDisplaySplash(false);
-      localStorage.setItem('__block_splash', '1');
+      localStorage.setItem(localStorageItemName, appUID);
     }, 1800);
 
     return () => {
@@ -30,7 +34,7 @@ const Splash = () => {
         <div className="fixed bg-grey-two flex justify-center items-center h-screen w-screen absolute z-50">
           <div className="text-center">
             <div className="mb-24">
-              <Lottie animationData={splashAnimation} style={{ width: '125px' }} loop={false}  />
+              <Lottie animationData={splashAnimation} style={{ width: '125px' }} loop={false} />
             </div>
             <div className="absolute left-0 right-0 margin-auto bottom-20 text-white">Minima Block 2.0.5</div>
           </div>
